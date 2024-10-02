5a1996d645eec9df63deb2a1e15f8ea69eea1e7f
SerZeMat <secemacorp@gmail.com>
2024-10-02 19:08:01 +0000

    Generador de excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
 /* eslint-disable */
-import "bootstrap";
+
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generateExcuse();
 };
+// Creamos arrays con la excusas
+
+const who = ["El perro", "Mi abuela", "El cartero", "Mi tortuga"];
+const action = ["se comió", "meó", "aplastó", "rompió"];
+const what = ["mis deberes", "mi teléfono", "el coche"];
+const when = [
+  "antes de la clase",
+  "mientras dormía",
+  "mientras hacía ejercicio",
+  "durante mi comida",
+  "mientras estaba conduciendo"
+];
+
+//Creamos el generador con el principio de una sola responsabilidad
+
+const getRandom = arra_y => Math.floor(Math.random() * arra_y.length);
+const getValue = arra_y => arra_y[getRandom(arra_y)];
+const generateExcuse = () =>
+  `${getValue(who)} ${getValue(action)} ${getValue(what)} ${getValue(when)}`;
+
+//Sacamos en pantalla
+
+console.log(getRandom(who));
+console.log(getRandom(action));
+console.log(getRandom(what));
+console.log(getRandom(when));
+
+console.log(
+  `${getValue(who)} ${getValue(action)} ${getValue(what)} ${getValue(when)}`
+);
